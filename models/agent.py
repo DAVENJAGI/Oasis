467372e508d40663649cea1dab4ea64b11688752
robocode 6 <robocode@robocode.robocode.com>
2025-05-01 10:57:07 -0400

    Agent model

---
+++ models/agent.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" holds class agents"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, Boolean
+from sqlalchemy.orm import relationship
+
+
+class Agent(BaseModel, Base):
+    """Representation of an agent"""
+    if models.storage_t == 'db':
+        __tablename__ = 'agents'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        telephone_no = Column(String(64), nullable=False)
+        sex = Column(String(64), nullable=False)
+        bio = Column(String(1024), nullable=False)
+        is_verified = Column(Boolean, default=False, nullable=False)
+        places = relationship("Listing", backref="agent")
+        ratings = relationship("agentReview", backref="agent")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        telephone_no = ""
+        sex = ""
+        is_verified = False
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
