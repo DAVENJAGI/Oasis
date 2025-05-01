9a6968308974e12543bdbd455c273fbafa2e66e6
robocode 6 <robocode@robocode.robocode.com>
2025-05-01 10:59:12 -0400

    Favorite listing model

---
+++ models/favorite_listing.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" holds favoriteProperty"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class favoriteListing(BaseModel, Base):
+    """Representation user's favorite properties"""
+    if models.storage_t == "db":
+        __tablename__ = 'favorite_listings'
+        listing_id = Column(String(64), ForeignKey("listings.id"), nullable=False)
+        user_id = Column(String(64), ForeignKey("users.id"), nullable=False)
+    else:
+        user_id = ""
+        listing_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
