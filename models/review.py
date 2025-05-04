d12d4dc68d007ab4d303af3b9dd97d05508e6a6a
robocode 6 <robocode@robocode.robocode.com>
2025-05-04 19:02:13 -0400

    review model listing id addition

--- models/review.py
+++ models/review.py
@@ -11,7 +11,7 @@ class Review(BaseModel, Base):
     """Representation of Review """
     if models.storage_type == 'db':
         __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        listing_id = Column(String(60), ForeignKey('listings.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
     else:
