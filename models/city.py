3e8d75136167988c6d4eaa7e5a2ce4315488789f
robocode 6 <robocode@robocode.robocode.com>
2025-05-04 10:03:36 -0400

    City model file

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-""" holds class City"""
+#!/usr/bin/python3
+""" holds class city"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -9,15 +9,15 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
+    """Representation of constituency class """ 
+    if models.storage_type == "db":
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        constituency_name = Column(String(128), nullable=False)
+        wards = relationship("Town", backref="cities")
     else:
+        constituency_name =""
         state_id = ""
-        name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes city"""
