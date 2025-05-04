d143436a4bf3ede07f7336af821e8651015ff9b2
robocode 6 <robocode@robocode.robocode.com>
2025-05-04 09:57:21 -0400

    Cities endpoint setup

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,9 +7,9 @@ from models import storage
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
-from models.place import Place
+from models.listing import Listing 
 from models.review import Review
-from models.state import State
+# from models.state import State
 from models.user import User
 import json
 
