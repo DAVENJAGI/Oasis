20f5802e3162ccee41533603b9d8b0c32cc762ca
robocode 6 <robocode@robocode.robocode.com>
2025-05-04 09:58:26 -0400

    inti file setup

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,15 @@ from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+city_views = Blueprint('city_views', __name__, url_prefix='/api/v1')
+state_views = Blueprint('state_views', __name__, url_prefix='/api/v1')
+ward_views = Blueprint('ward_views', __name__, url_prefix='/api/v1')
+listing_views = Blueprint('listing_views', __name__, url_prefix='/api/v1')
+user_views = Blueprint('user_views', __name__, url_prefix='/api/v1')
+agent_views = Blueprint('agent_views', __name__, url_prefix='/api/v1')
+favorite_views = Blueprint('favorite_views', __name__, url_prefix='/api/v1')
+amenity_views = Blueprint('amenity_views', __name__, url_prefix='/api/v1')
+
 
 
 from api.v1.views.index import *
