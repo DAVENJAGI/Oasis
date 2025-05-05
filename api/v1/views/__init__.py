ea016433f379c5274d5df7031cf6b295f3eac753
robocode 6 <robocode@robocode.robocode.com>
2025-05-05 10:32:10 -0400

    Register agent_view blueprint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,6 +10,7 @@ ward_views = Blueprint('ward_views', __name__, url_prefix='/api/v1')
 listing_views = Blueprint('listing_views', __name__, url_prefix='/api/v1')
 user_views = Blueprint('user_views', __name__, url_prefix='/api/v1')
 agent_views = Blueprint('agent_views', __name__, url_prefix='/api/v1')
+agent_views = Blueprint('agent_views', __name__, url_prefix='/api/v1')
 favorite_views = Blueprint('favorite_views', __name__, url_prefix='/api/v1')
 amenity_views = Blueprint('amenity_views', __name__, url_prefix='/api/v1')
 
@@ -20,6 +21,7 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
+from api.v1.views.agents import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
