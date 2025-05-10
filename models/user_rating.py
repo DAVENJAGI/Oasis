9591645cb433f81b5233ebb376f8760dd39b7856
robocode 6 <robocode@robocode.robocode.com>
2025-05-10 08:35:18 -0400

    modifying user rating model

--- models/user_rating.py
+++ models/user_rating.py
@@ -4,18 +4,20 @@ Contains the userRating class
 """
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Float
 import models
 
 class userRating(BaseModel, Base):
     """Representation of a user rating"""
     if models.storage_type == 'db':
         __tablename__ = 'user_ratings'
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        score = Column(Integer, nullable=False)
-        comment = Column(String(1024), nullable=True)
+        user_id = Column(String(64), ForeignKey('users.id'), nullable=False)
+        agent_id = Column(String(64), ForeignKey('agents.id'), nullable=False)
+        score = Column(Float, nullable=False)
+        user_comment = Column(String(1024), nullable=True)
     else:
         user_id = ""
+        agent_id = ""
         score = 0
         comment = ""
     
