70be31bf9c9e057f46c4aa0f5cbf7189892667b0
robocode 6 <robocode@robocode.robocode.com>
2025-05-10 08:38:31 -0400

    modifying lease model

--- models/lease.py
+++ models/lease.py
@@ -5,8 +5,9 @@ from models.base_model import BaseModel, Base
 from models.city import City
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Date, Boolean
 from sqlalchemy.orm import relationship
+from sqlalchemy.types import Enum
 
 
 class Lease(BaseModel, Base):
@@ -20,6 +21,9 @@ class Lease(BaseModel, Base):
         stop_date = Column(Date, nullable=False)
         payment_per_night = Column(Float, default=0, nullable=False)
         status = Column(Enum("pending", "active", "terminated", name="lease_status_enum"), default="pending")
+        terms_and_conditions = Column(String(1024))
+        signed_by_lessee = Column(Boolean, default=False, nullable=False)
+        signed_by_lessor = Column(Boolean, default=False, nullable=False)
     else:
         user_id = ""
         listing_id = ""
