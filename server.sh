28ee711ae45a68da8949f53f641366b2a57ed576
robocode 6 <robocode@robocode.robocode.com>
2025-05-10 08:47:01 -0400

    other files

--- server.sh
+++ server.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-read -sp "Enter MySQL password: " MYSQL_PWD
-echo
+
+# read -sp "Enter MySQL password: " MYSQL_PWD
+echo "Starting Flask server... "
 
 export HBNB_MYSQL_USER="oasis_admin"
-export HBNB_MYSQL_PWD="$MYSQL_PWD"
+export HBNB_MYSQL_PWD="  "
 export HBNB_MYSQL_HOST="localhost"
 export HBNB_MYSQL_DB="oasis"
 export HBNB_ENV="0.0.0.0"
 export HBNB_TYPE_STORAGE="db"
 
 python3 -m api.v1.app
-
-
