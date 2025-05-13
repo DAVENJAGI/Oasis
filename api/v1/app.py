6bc3231808eee9cc4298eec0dd7438819809eec3
robocode 6 <robocode@robocode.robocode.com>
2025-05-13 18:55:01 -0400

    Register tag_views blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ from api.v1.views import listing_views, agent_views
 from api.v1.views import amenity_views, user_views, location_views
 from api.v1.views import ticket_views, admin_views, lease_views
 from api.v1.views import review_views, report_views, recommendation_views
+from api.v1.views import tag_views
 from models import storage
 import os
 from flask import jsonify
@@ -27,6 +28,7 @@ app.register_blueprint(lease_views)
 app.register_blueprint(review_views)
 app.register_blueprint(report_views)
 app.register_blueprint(recommendation_views)
+app.register_blueprint(tag_views)
 
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
