10c9133ff2c8e82ec6a43a922c6a76178d6da1af
robocode 6 <robocode@robocode.robocode.com>
2025-05-13 18:53:49 -0400

    Create save cover photo function

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -12,3 +12,16 @@ def save_image(file, listing_id):
     filepath = os.path.join(UPLOAD_FOLDER, filename)
     file.save(filepath)
     return filepath
+
+COVER_UPLOAD_FOLDER = 'static/images/listings/cover_photos'
+
+def save_cover_image(file, listing_id):
+    """Saves a cover photo file locally and returns the storage location file path"""
+    if not os.path.exists(COVER_UPLOAD_FOLDER):
+        os.makedirs(COVER_UPLOAD_FOLDER)
+
+    filename = secure_filename(f"{listing_id}_{file.filename}")
+    filepath = os.path.join(COVER_UPLOAD_FOLDER, filename)
+    file.save(filepath)
+    return filepath
+
