fc7ea21cb583f31787a1702daaedea7e38e359de
robocode 6 <robocode@robocode.robocode.com>
2025-05-14 17:43:42 -0400

    Support agent session model

---
+++ models/support_agent_session.py
@@ -0,0 +1,28 @@
+#!usr/bin/python3
+"""This is a class containing the support_agent session details"""
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey, Table, Integer, DateTime
+from sqlalchemy.orm import relationship
+import models
+from datetime import datetime, timedelta
+
+class supportAgent(BaseModel, Base):
+    """Represents support_agent session details"""
+
+    if models.storage_type == 'db':
+        __tablename__ = 'support_agent_sessions'
+        support_agent_id = Column(Integer, ForeignKey('support_agents.id'), nullable=False)
+        session_token = Column(String(128), nullable=False)
+        authorization_token = Column(String(128), nullable=False)
+        expires_at = Column(DateTime, nullable=False)
+        support_agents = relationship("supportAgent", backref="support_agent_sessions")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.expires_at = datetime.utcnow() + timedelta(hours=1)
+
+    @property
+    def is_valid(self):
+        return self.expires_at > datetime.utcnow()
