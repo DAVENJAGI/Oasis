6511235d4c656152e48eb732a038dcee751543f1
robocode 6 <robocode@robocode.robocode.com>
2025-05-14 18:11:52 -0400

    login_views blueprint registration

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from api.v1.views import listing_views, agent_views
 from api.v1.views import amenity_views, user_views, location_views
 from api.v1.views import ticket_views, admin_views, lease_views
 from api.v1.views import review_views, report_views, recommendation_views
-from api.v1.views import tag_views, support_agent_views
+from api.v1.views import tag_views, support_agent_views, login_views
 from models import storage
 import os
 from flask import jsonify
@@ -30,6 +30,7 @@ app.register_blueprint(report_views)
 app.register_blueprint(recommendation_views)
 app.register_blueprint(tag_views)
 app.register_blueprint(support_agent_views)
+app.register_blueprint(login_views)
 
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
