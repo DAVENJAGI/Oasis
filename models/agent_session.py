9756bea0ecaa6e5f9914887e6d15661a68a8e812
robocode 6 <robocode@robocode.robocode.com>
2025-05-14 17:43:14 -0400

    Agent session model

---
+++ models/agent_session.py
@@ -0,0 +1,29 @@
+#!usr/bin/python3
+"""This is a class containing the agent session details"""
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey, Table, Integer, DateTime
+from sqlalchemy.orm import relationship
+import models
+from datetime import datetime, timedelta
+
+class agentSession(BaseModel, Base):
+    """Represents agent session details"""
+
+    if models.storage_type == 'db':
+        __tablename__ = 'agent_sessions'
+        agent_id = Column(Integer, ForeignKey('agents.id'), nullable=False)
+        session_token = Column(String(128), nullable=False)
+        authorization_token = Column(String(128), nullable=False)
+        expires_at = Column(DateTime, nullable=False)
+        agents = relationship("Agent", backref="agent_sessions")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.expires_at = datetime.utcnow() + timedelta(hours=1)
+
+    @property
+    def is_valid(self):
+        return self.expires_at > datetime.utcnow()
+
