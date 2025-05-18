97bbe954573de7a7320f3e3a37874111b87856ff
robocode 6 <robocode@robocode.robocode.com>
2025-05-18 10:27:48 -0400

    User endpoint authorization

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -11,19 +11,21 @@ from models.favorite_listing import favoriteListing
 from models.listing import Listing
 from flasgger.utils import swag_from
 from werkzeug.security import generate_password_hash
-
+from auth.authorization import require_admin_auth, require_user_or_admin_auth, require_agent_or_admin_or_user_auth, require_support_agent_or_admin_or_user_auth
 
 @user_views.route('/users', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/user/get.yml', methods=['GET'])
+@require_admin_auth
 def get_all_users():
     """ get users by id"""
     all_list = [obj.to_dict() for obj in storage.all(User).values()]
     return jsonify(all_list)
 
 
-@user_views.route('/users/<string:user_id>', methods=['GET'],
+@user_views.route('/user/<string:user_id>', methods=['GET'],
                  strict_slashes=False)
 @swag_from('documentation/user/get_id.yml', methods=['GET'])
+@require_user_or_admin_auth
 def get_user(user_id):
     """ get user by id"""
     user = storage.get(User, user_id)
@@ -33,9 +35,10 @@ def get_user(user_id):
     return jsonify(user.to_dict())
 
 
-@user_views.route('/users/<string:user_id>', methods=['DELETE'],
+@user_views.route('/user/<string:user_id>', methods=['DELETE'],
                  strict_slashes=False)
 @swag_from('documentation/user/delete.yml', methods=['DELETE'])
+@require_user_or_admin_auth
 def del_user(user_id):
     """ delete user by id"""
     user = storage.get(User, user_id)
@@ -75,9 +78,10 @@ def create_obj_user():
     return (jsonify(obj.to_dict()), 201)
 
 
-@user_views.route('/users/<string:user_id>', methods=['PUT'],
+@user_views.route('/user/<string:user_id>', methods=['PUT'],
                  strict_slashes=False)
 @swag_from('documentation/user/put.yml', methods=['PUT'])
+@require_user_or_admin_auth
 def put_user(user_id):
     """ put user data endpoint  """
     if not request.form:
@@ -96,9 +100,10 @@ def put_user(user_id):
     return jsonify(obj.to_dict())
 
 ''' USER FAVORITE LISTINGS '''
-@user_views.route('/users/<string:user_id>/favorites/', methods=['POST'],
+@user_views.route('/user/<string:user_id>/favorites/', methods=['POST'],
                  strict_slashes=False)
 @swag_from('documentation/favorites/post.yml', methods=['POST'])
+@require_user_or_admin_auth
 def create_obj_favorites(user_id):
     """ create new instance of user favorite listings"""
     if not request.get_json():
@@ -123,6 +128,7 @@ def create_obj_favorites(user_id):
 @user_views.route('/user/<string:user_id>/favorites',
                  methods=['GET'], strict_slashes=False)
 @swag_from('documentation/favorites/get.yml', methods=['GET'])
+@require_user_or_admin_auth
 def get_all_user_favorites(user_id):
     """ user favorite listings """
     user = storage.get(User, user_id)
@@ -132,9 +138,10 @@ def get_all_user_favorites(user_id):
     return jsonify(favorites)
 
 
-@user_views.route('/users/<string:user_id>/rating/', methods=['POST'],
+@user_views.route('/user/<string:user_id>/rating/', methods=['POST'],
                  strict_slashes=False)
 @swag_from('documentation/ratings/post.yml', methods=['POST'])
+@require_agent_or_admin_or_user_auth
 def create_obj_user_ratings(user_id):
     """ create new instance of user ratings"""
     if not request.get_json():
@@ -159,6 +166,7 @@ def create_obj_user_ratings(user_id):
 @user_views.route('/user/<string:user_id>/ratings',
                  methods=['GET'], strict_slashes=False)
 @swag_from('documentation/ratings/get.yml', methods=['GET'])
+@require_agent_or_admin_or_user_auth
 def get_all_user_ratings(user_id):
     """ user rating """
     user = storage.get(User, user_id)
@@ -171,6 +179,7 @@ def get_all_user_ratings(user_id):
 @user_views.route('/user/<string:user_id>/leases',
                  methods=['GET'], strict_slashes=False)
 @swag_from('documentation/leases/get.yml', methods=['GET'])
+@require_user_or_admin_auth
 def get_all_user_leases(user_id):
     """ user leases """
     user = storage.get(User, user_id)
@@ -184,6 +193,7 @@ def get_all_user_leases(user_id):
 @user_views.route('/user/<string:user_id>/reviews',
                  methods=['GET'], strict_slashes=False)
 @swag_from('documentation/reviews/get.yml', methods=['GET'])
+@require_agent_or_admin_or_user_auth
 def get_all_user_reviews(user_id):
     """ user reviews """
     user = storage.get(User, user_id)
@@ -196,6 +206,7 @@ def get_all_user_reviews(user_id):
 @user_views.route('/user/<string:user_id>/tickets',
                  methods=['GET'], strict_slashes=False)
 @swag_from('documentation/tickets/get.yml', methods=['GET'])
+@require_support_agent_or_admin_or_user_auth
 def get_all_user_tickets(user_id):
     """ user tickets """
     user = storage.get(User, user_id)
