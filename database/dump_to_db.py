64d121a06684bbbac4a46c5dca127ec1f93f638b
DAVENJAGI <njagidave39@gmail.com>
2025-06-11 13:16:28 -0400

    Dump to db script

--- database/dump_to_db.py
+++ database/dump_to_db.py
@@ -1,28 +1,35 @@
+#!/usr/bin/env python3
+
 import os
 from os import getenv
-import pandas as pd
+import pymysql
 from sqlalchemy import create_engine
 
-password = os.getenv["MYSQL_USER_PASSWORD"]
-database = os.getenv["DATABASE_NAME"]
+password = os.getenv("MYSQL_USER_PASSWORD")
+database = os.getenv("DATABASE_NAME")
+user = os.getenv("DB_USER")
 
-engine = create_engine("mysql+pymysql://oasis_admin:password @localhost/database")
+conn = pymysql.connect(
+    host='localhost',
+    user=user,
+    password=password,
+    database=database,
+    autocommit=True
+)
 
-data_folder = "/"
-
+data_folder = "database_backups/"
 
 for filename in os.listdir(data_folder):
-    file_path = os.path.join(data_folder, filename)
-    
-    if filename.endswith(".csv"):
-        df = pd.read_csv(file_path)
-        table_name = os.path.splitext(filename)[0]
-        df.to_sql(table_name, con=engine, if_exists='append', index=False)
-        print(f"Loaded CSV file {filename} into table {table_name}")
-        
-    elif filename.endswith(".json"):
-        df = pd.read_json(file_path)
-        table_name = os.path.splitext(filename)[0]
-        df.to_sql(table_name, con=engine, if_exists='append', index=False)
-        print(f"Loaded JSON file {filename} into table {table_name}")
+    if filename.endswith('.sql'):
+        file_path = os.path.join(data_folder, filename)
+        with open(file_path, 'r') as f:
+            sql_commands = f.read()
+
+        with conn.cursor() as cursor:
+            for command in sql_commands.split(';'):
+                if command.strip():
+                    cursor.execute(command)
+            print(f"Executed SQL dump from {filename}")
+
+conn.close()
 
