e8120c8340028d67acc5d40831976fb9935f4a0c
DAVENJAGI <njagidave39@gmail.com>
2025-07-09 19:43:32 -0400

    Final commit of a working backend on server

--- server.sh
+++ server.sh
@@ -5,10 +5,10 @@
 echo "Starting Flask server... "
 
 export HBNB_MYSQL_USER="oasis_admin"
-export HBNB_MYSQL_PWD="  "
-export HBNB_MYSQL_HOST="localhost"
+export HBNB_MYSQL_PWD="keem"
+export HBNB_MYSQL_HOST="db"
 export HBNB_MYSQL_DB="oasis"
 export HBNB_ENV="0.0.0.0"
-export HBNB_TYPE_STORAGE="db"
+# export HBNB_TYPE_STORAGE="db"
 
 python3 -m api.v1.app
