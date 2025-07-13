03c7090302a7bdb75c8d07e09912c5c5566766c2
DAVENJAGI <njagidave39@gmail.com>
2025-07-12 22:01:48 -0400

    Rerouting to profile page handler

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,2 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const userProfileAvatar = document.getElementById('usr-profile-avatar');
+    userProfileAvatar.addEventListener('click', () => {
+        window.location.href = 'profile.html';
+    })
 });
